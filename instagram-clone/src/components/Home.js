58c69be2bae0385c9331809b148775c71f4874e7
BaoWoW <baonguyen9392@gmail.com>
2023-03-31 09:15:16 -0700

    Update Follower Stories & Posts
    Added slider buttons to stories and related event handlers to Home component.
    Posts have minor style changes.
    Restyled navBar to sticky positioning and updated rightDiv(suggestions) styling.

--- instagram-clone/src/components/Home.js
+++ instagram-clone/src/components/Home.js
@@ -1,36 +1,83 @@
 import "../styles/Home.css";
-import testImgArr from "./testImages";
-
+import { testImgArr, testPostArr } from "./testImages";
+import post from "../assets/navBar/post.png";
 const Home = () => {
+  const followers = document.querySelectorAll(".follower");
+
+  const showNext = async () => {
+    await followers.forEach((el) => {
+      el.style.transform = "translateX(-360px)";
+    });
+    const prevBtn = document.querySelector(".storiesPrevBtn");
+    const nextBtn = document.querySelector(".storiesNextBtn");
+    nextBtn.className = "storiesNextBtn hidden";
+    prevBtn.className = "storiesPrevBtn";
+  };
+
+  const showPrev = async () => {
+    await followers.forEach((el) => {
+      el.style.transform = "translateX(0px)";
+    });
+    const prevBtn = document.querySelector(".storiesPrevBtn");
+    const nextBtn = document.querySelector(".storiesNextBtn");
+    nextBtn.className = "storiesNextBtn";
+    prevBtn.className = "storiesPrevBtn hidden";
+  };
+
   return (
     <div className="Home">
       <div className="feed">
-        <div className="followers">
-          {testImgArr.map((el, i) => {
-            return (
-              <div className="follower" data-id={i}>
-                <div className="followerImg">
-                  <img src={el.img} />
+        <div className="storiesContainer">
+          <button className="storiesNextBtn" onClick={showNext}>
+            {">"}
+          </button>
+          <button className="storiesPrevBtn hidden" onClick={showPrev}>
+            {"< "}
+          </button>
+          <div className="storiesWrapper">
+            {/* slider-wrapper */}
+
+            {testImgArr.map((el, i) => {
+              return (
+                <div className="follower" data-id={i}>
+                  {" "}
+                  {/* slider */}
+                  <div className="followerImg">
+                    <img src={el.img} />
+                  </div>
+                  <div>{el.username}</div>
                 </div>
-                <div>{el.username}</div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
         <div className="posts">
           <div>
             <h1>Home</h1>
           </div>
-          <div>test</div>
-          <div>anoterh test</div>
+          {testPostArr.map((el) => {
+            return (
+              <div className="post">
+                <div>
+                  <div className="postInfo">
+                    <img src={el.img} />
+                    <div style={{ fontWeight: "bold" }}>{el.username}</div>
+                  </div>
+                  <img src={post} />
+                </div>
+                <img src={el.post} />
+                <div className="postDescrip">hola</div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className="rightDiv">
         <div>
           <div className="suggestSwitchAcc">
             <img src={testImgArr[0].img} />
-            <div >
-              <div >Account Username</div>
+            <div>
+              <div>Account Username</div>
               <div>Some username</div>
             </div>
           </div>
@@ -38,7 +85,8 @@ const Home = () => {
         </div>
         <div className="suggestions">
           <div>
-            Suggestions For You <button style={{color:'black'}}>See All</button>
+            Suggestions For You{" "}
+            <button style={{ color: "black" }}>See All</button>
           </div>
           {testImgArr.map((el, i) => {
             return (
--- instagram-clone/src/components/testImages.js
+++ instagram-clone/src/components/testImages.js
@@ -4,12 +4,25 @@ import follower3 from "../assets/testImages/flick.jpg";
 import follower4 from "../assets/testImages/heimlich.jpg";
 import follower5 from "../assets/testImages/hopper.jpg";
 import follower6 from "../assets/testImages/night.jpg";
-
+import post from '../assets/testImages/rockWall.jpg'
+import post1 from '../assets/testImages/rockWallBlack.jpg'
 const testImgArr = [
     {username:'rosie',img:follower},
-    {username:'dot',img:follower2},
-    {username:'flick',img:follower3},
-    {username:'heimlich',img:follower4},
+    {username:'dot',img:follower2, post:post },
+    {username:'flick',img:follower3, post:post1},
+    {username:'heimlich',img:follower4,post:post},
     {username:'hopper',img:follower5},
-    {username:'night',img:follower6}]
-export default testImgArr;
+    {username:'rosie',img:follower},
+    {username:'dot',img:follower2, post:post },
+    {username:'flick',img:follower3, post:post1},
+    {username:'heimlich',img:follower4,post:post},
+    {username:'hopper',img:follower5},
+    {username:'night',img:follower6}
+]
+
+    const testPostArr = [
+        {username:'dot',img:follower2, post:post },
+        {username:'flick',img:follower3, post:post1},
+        {username:'heimlich',img:follower4,post:post}]
+
+export { testImgArr, testPostArr};
