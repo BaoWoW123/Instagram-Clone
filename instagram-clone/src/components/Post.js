33535d62e11dcab0303cd667367e5b74b914168a
BaoWoW <baonguyen9392@gmail.com>
2023-04-13 16:28:04 -0700

    Update Create Component
    Create component renders separate page rather than appearing on initial component like Instagram.
    User can drag and drop file or manually select file to upload.
    Both methods are implemented, but do not save image/file uploaded or signify the file is uploaded.
    When upload button is click, method uploadFile() runs twice due to click on input inside of the function called img.click().

--- instagram-clone/src/components/Post.js
+++ instagram-clone/src/components/Post.js
@@ -1,11 +1,70 @@
 import "../styles/Post.css";
 import NavBar from "./NavBar";
+import images from "../assets/navBar/images.png";
 
 const Post = (props) => {
+  const uploadFile = (e) => { //RUNS TWICE, html overlaps
+    const img = document.querySelector('.imgUpload')
+    img.click()
+  };
+
+  const updateFile = () => {
+    console.log('updated files')
+  }
+  const dropHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("file dropped")
+    if (e.target.className === "uploadForm") {
+      return (e.target.style.backgroundColor = "white");
+    }
+  };
+
+  const dragEnter = (e) => {
+    const div = e.target;
+    if (div.className === "uploadForm")
+      return (div.style.backgroundColor = "lightgray");
+  };
+
+  const dragExit = (e) => {
+    const div = e.target;
+    if (div.className === "uploadForm")
+      return (div.style.backgroundColor = "white");
+  };
+
   return (
-    <div className="Post">
-      <NavBar signOut={props.signOut}/>
-      <h1>Post</h1>
+    <div className="postPage" aria-label="postPage">
+      <NavBar signOut={props.signOut} />
+      <div className="createPost">
+        <span>Create new post</span>
+        <form
+          onDrop={dropHandler}
+          onDragEnter={dragEnter}
+          onDragLeave={dragExit}
+          onDragOver={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+          onChange={(e)=> {e.preventDefault()}}
+          className="uploadForm"
+        >
+          <img src={images} className="uploadImg" />
+          Drag photos and videos here
+          <button
+            className="customUpload"
+            type='button'
+            onClick={uploadFile}
+            >
+            Select from your computer
+            <input
+              type="file"
+              className="imgUpload"
+              onChange={updateFile}
+              style={{ display: "none" }}
+            />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
